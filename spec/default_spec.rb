faae90f32ec3f788f8f55c80853637b9333affc7
wate <pc.poisoning@gmail.com>
2018-03-14 01:39:23 +0900

    Squashed 'provision/roles/repo-remi/' changes from 1282aae..2a68ec9
    2a68ec9 テストを調整
    94402c3 依存関係を追記
    d3f4f66 テスト周りを調整
    
    git-subtree-dir: provision/roles/repo-remi
    git-subtree-split: 2a68ec9e324c76c5b69153dc9adcdfccd83b7fe5

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,14 +1,13 @@
 require 'spec_helper'
 
-describe 'role repo-remi' do
-  describe package('remi-release') do
-    it { should be_installed }
-  end
-  describe yumrepo('remi') do
-    it { should exist }
-  end
+describe package('remi-release') do
+  it { should be_installed }
+end
+
+describe yumrepo('remi') do
+  it { should exist }
   describe command("yum repolist enabled -C|awk '{print $1}' | grep -E '^remi$'") do
-    if property["repo_remi_enabled"]
+    if property['repo_remi_enabled']
       its(:stdout) { should contain('remi') }
     else
       its(:stdout) { should_not contain('remi') }
