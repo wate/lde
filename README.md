6da6410572c47b4665bf1de6c34751f8f30a424b
wate <pc.poisoning@gmail.com>
2018-03-14 23:38:28 +0900

    Squashed 'provision/roles/common/' changes from 71a22ed..03d0c3a
    03d0c3a テストを修正
    5032f46 テストを更新
    05843c4 テストを追記
    b80ee23 テストを修正
    
    git-subtree-dir: provision/roles/common
    git-subtree-split: 03d0c3a71c6a65e9cacff6dd2f3d7818a070d1b7

--- README.md
+++ README.md
@@ -1,24 +1,108 @@
 common
 =========
 
-ユーザーの追加や基本的なパッケージのインストールなどの共通処理のセットアップを行います
+ユーザーの追加や基本パッケージのインストールなど、サーバーの共通セットアップ処理を行います
 
 Role Variables
 --------------
 
-A description of the settable variables for this role should go here, including any variables that are in defaults/main.yml, vars/main.yml, and any variables that can/should be set via parameters to the role. Any variables that are read from other roles and/or the global scope (ie. hostvars, group vars, etc.) should be mentioned here as well.
+### common_groups
 
-Dependencies
-------------
+サーバーに登録/削除するグループを指定します。
 
-* [repo-epel](https://github.com/wate/ansible-role-repo-epel)
+```yaml
+common_groups:
+  - name: group1
+  - name: group2
+    remove: yes
+```
+
+### common_users
+
+サーバーに登録/削除するユーザーを指定します。
+
+```yaml
+common_users:
+  - name: sample_user
+    password: "{{ 'password'|password_hash('sha512') }}"
+    groups:
+      - group1
+      - group2
+    shell: /bin/nologin
+    admin: no
+- name: admin_user
+  password: "{{ 'password'|password_hash('sha512') }}"
+  admin: yes
+  groups:
+    - group1
+    - group2
+  authorized_keys:
+    - "{{ lookup('file', '~/.ssh/id_rsa.pub') }}"
+    - https://github.com/ghuser.keys
+- name: remove_user
+  password: "{{ 'password'|password_hash('sha512') }}"
+  groups:
+    - group1
+    - group2
+  authorized_keys:
+    - "{{ lookup('file', '~/.ssh/id_rsa.pub') }}"
+    - https://github.com/ghuser.keys
+  shell: /bin/nologin
+  remove: yes
+```
+
+### common_packages
+
+インストールする基本パッケージを指定します
+
+```yaml
+common_packages:
+  - unzip
+  - zip
+  - perl
+  - cpp
+  - make
+  - autoconf
+  - automake
+  - diffstat
+  - m4
+  - libtool
+  - gcc
+  - gcc-c++
+  - patch
+  - git
+  - yum-utils
+  - vim-enhanced
+  - bash-completion
+  - tig
+```
+
+### fail2ban_jail_cfg
+
+fail2banの設定を定義します。
+
+```yaml
+fail2ban_jail_cfg:
+  sshd:
+    enabled: yes
+```
+
+### sudo_require_password
+
+sudoで実行を行う場合にパスワードを求めるか否かを指定します。
+
+```yaml
+sudo_require_password: no
+```
 
 Example Playbook
 ----------------
 
-    - hosts: servers
-      roles:
-         - { role: common }
+```yaml
+- hosts: servers
+  roles:
+     - { role: common }
+```
 
 License
 -------
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,28 +1,79 @@
 require 'spec_helper'
+require 'open-uri'
 
-describe 'role common' do
-  property['common_packages'].each do |pkg|
-    describe package(pkg) do
-      it { should be_installed }
+describe file('/etc/skel/public') do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 755 }
+end
+
+describe file('/home') do
+  it { should be_mode 755 }
+end
+
+describe file('/etc/sudoers.d/wheel') do
+  it { should exist }
+  it { should be_file }
+  if property['sudo_require_password']
+    it { should contain '%wheel ALL=(ALL) ' }
+    it { should_not contain '%wheel ALL=(ALL) NOPASSWD: ALL' }
+  else
+    it { should contain '%wheel ALL=(ALL) NOPASSWD: ALL' }
+  end
+end
+
+property['common_groups'].each do |group|
+  describe group(group['name']) do
+    is_removed = group.key?('remove') && group['remove']
+    if is_removed
+      it { should_not exist }
+    else
+      it { should exist }
     end
   end
+end
 
-  describe package('firewalld') do
-    it { should be_installed }
+property['common_users'].each do |user|
+  describe user(user['name']) do
+    is_removed = user.key?('remove') && user['remove']
+    if is_removed
+      it { should_mot exist }
+    else
+      it { should exist }
+      it { should have_login_shell user['shell'] } if user.key?('shell')
+      if user.key?('groups')
+        user['groups'].each do |group_name|
+          it { should belong_to_group group_name }
+        end
+      end
+    end
   end
-  describe package('etckeeper') do
-    it { should be_installed }
-  end
-  describe package('fail2ban') do
-    it { should be_installed }
-  end
+end
 
-  describe service('firewalld') do
-    it { should be_enabled }
-    it { should be_running }
+property['common_packages'].each do |pkg|
+  describe package(pkg) do
+    it { should be_installed }
   end
-  describe service('fail2ban') do
-    it { should be_enabled }
-    it { should be_running }
-  end
+end
+
+describe package('firewalld') do
+  it { should be_installed }
+end
+
+describe package('etckeeper') do
+  it { should be_installed }
+end
+
+describe package('fail2ban') do
+  it { should be_installed }
+end
+
+describe service('firewalld') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service('fail2ban') do
+  it { should be_enabled }
+  it { should be_running }
 end
