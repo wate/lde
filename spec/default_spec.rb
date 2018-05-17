b59aada27b4e9627682c95e540afd60f27d27d5b
wate <pc.poisoning@gmail.com>
2018-05-18 04:08:03 +0900

    Squashed 'provision/roles/common/' changes from c3b388e..8009b10
    8009b10 ホームディレクトリの指定に対応
    c8d6db9 テストコードを修正
    2a76aa7 ユーザーディレクトリ周りの処理およびユーザー・グループ管理周りの機能にIDして機能を追加
    
    git-subtree-dir: provision/roles/common
    git-subtree-split: 8009b10568f132abba40012b9d63e2f2eb62e6af

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,11 +1,5 @@
 require 'spec_helper'
 
-describe file('/etc/skel/public') do
-  it { should exist }
-  it { should be_directory }
-  it { should be_mode 755 }
-end
-
 describe file('/home') do
   it { should be_mode 755 }
 end
