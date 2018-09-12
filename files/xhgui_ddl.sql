3e01d7cbddc520ad6ef65aa09f7e9461aa7575df
wate <pc.poisoning@gmail.com>
2018-09-13 04:59:43 +0900

    Squashed 'provision/roles/xhgui/' changes from dab8b5e..15053bd
    15053bd xhguiのDDLを修正
    
    git-subtree-dir: provision/roles/xhgui
    git-subtree-split: 15053bd0af0bf25237c6d2bfddc7dba03e64dc32

--- files/xhgui_ddl.sql
+++ files/xhgui_ddl.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS `details` (
   `pmu` int(11) unsigned default NULL,
   `wt` int(11) unsigned default NULL,
   `cpu` int(11) unsigned default NULL,
-  `server_id` char(3) NOT NULL default 't11',
+  `server_id` char(20) NOT NULL default 't11',
   `aggregateCalls_include` varchar(255) DEFAULT NULL,
   PRIMARY KEY  (`id`),
   KEY `url` (`url`),
