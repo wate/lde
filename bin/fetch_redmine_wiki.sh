4f3928b1d7b5ada260dee220bfb02b10d2684e6c
wate <pc.poisoning@gmail.com>
2023-03-06 06:59:19 +0900

    ファイル名を変更

--- bin/fetch_redmine_wiki.sh
+++ bin/fetch_redmine_wiki.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_PATH=$(realpath $0)
 BIN_DIR=$(dirname $SCRIPT_PATH)
-ansible-playbook -i 127.0.0.1, -c local ${BIN_DIR}/fetch_wiki.yml
+ansible-playbook -i 127.0.0.1, -c local ${BIN_DIR}/fetch_redmine_wiki.yml
