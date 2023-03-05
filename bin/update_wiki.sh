03477f945a90f8834ac7e434880aaf052ccc1d1f
wate <pc.poisoning@gmail.com>
2023-03-06 03:23:40 +0900

    変数を分離しファイル構造を調整

--- bin/update_wiki.sh
+++ bin/update_wiki.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-WORKING_DIR=${PWD}
 SCRIPT_PATH=$(realpath $0)
 BIN_DIR=$(dirname $SCRIPT_PATH)
-ROOT_DIR=$(realpath ${BIN_DIR}/../)
-if [ "${ROOT_DIR}" != "${WORKING_DIR}" ]; then
-    cd ${ROOT_DIR} || return
-fi
-ansible-playbook -i 127.0.0.1, -c local ${BIN_DIR}/update_wiki.yml --extra-vars "{root_dir: ${ROOT_DIR}}"
-if [ "${ROOT_DIR}" != "${WORKING_DIR}" ]; then
-    cd ${WORKING_DIR}  || return
-fi
+ansible-playbook -i 127.0.0.1, -c local ${BIN_DIR}/update_wiki.yml
